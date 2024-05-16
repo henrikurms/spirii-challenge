603fc8e18d8ccc347c70d83c386244f1ddd65938
Henrik Urms <urmshenrik@gmail.com>
2024-05-16 15:57:18 +0200

    Done for now

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,61 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Param, Query } from "@nestjs/common";
+import { AppService, UserTransactionData } from "./app.service";
+import { AggregatedUserData } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('users/:id')
+  getAggregatedUserData(
+    @Param('id') userId: string,
+  ): AggregatedUserData | undefined {
+    return this.appService.getAggregatedUserData(userId);
+  }
+
+  @Get('payouts')
+  getUserPayouts() {
+    return this.appService.getUsersPayouts();
+  }
+
+  // This endpoint should be in the transaction service, here for POC purposes
+  @Get('transactions')
+  getTransactionData(
+    // @Query('startDate') _startDate?: string,
+  // @Query('endDate') _endDate?: string,
+  ) {
+    const data: UserTransactionData = {
+      items: [
+        {
+          id: '41bbdf81-735c-4aea-beb3-3e5f433a30c5',
+          userId: '074092',
+          createdAt: '2023-03-16T12:33:11.000Z',
+          type: 'payout',
+          amount: 30,
+        },
+        {
+          id: '41bbdf81-735c-4aea-beb3-3e5fasfsdfef',
+          userId: '074092',
+          createdAt: '2023-03-12T12:33:11.000Z',
+          type: 'spent',
+          amount: 12
+        },
+        {
+          id: '41bbdf81-735c-4aea-beb3-342jhj234nj234',
+          userId: '074092',
+          createdAt: '2023-03-15T12:33:11.000Z',
+          type: 'earned',
+          amount: 1.2,
+        },
+      ],
+      meta: {
+        totalItems: 1200,
+        itemCount: 3,
+        itemsPerPage: 3,
+        totalPages: 400,
+        currentPage: 1,
+      },
+    }
+    return data;
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,101 @@
 import { Injectable } from '@nestjs/common';
+import { Cron } from '@nestjs/schedule';
+import { HttpService } from '@nestjs/axios';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly httpService: HttpService) {}
+
+  //TODO: Should be replaced with a real database
+  userStore: Map<string, AggregatedUserData> = new Map();
+
+  getAggregatedUserData(userId: string): AggregatedUserData | undefined {
+    return this.userStore.get(userId);
   }
+
+  getUsersPayouts(): Array<UserPayout> {
+    return [];
+  }
+
+  // 5 times every minute, i.e. every 12 seconds
+  @Cron('*/12 * * * * *')
+  async updateTransactionData() {
+    const { data } = await firstValueFrom(
+      this.httpService
+        .get<UserTransactionData>('http://localhost:3000/transactions')
+        .pipe(),
+    );
+
+    // @ts-ignore
+    const grouped: { [key: string]: UserTransaction[] } = Object.groupBy(
+      data.items,
+      (item: UserTransaction) => item.userId,
+    );
+    for (const [key, value] of Object.entries(grouped)) {
+      const userData = this.userStore.get(key) ?? {
+        balance: 0,
+        earned: 0,
+        spent: 0,
+        payOut: 0,
+        paidOut: 0,
+      };
+      for (const entry of value) {
+        switch (entry.type) {
+          case 'earned':
+            userData.earned += entry.amount;
+            break;
+          case 'spent':
+            userData.spent += entry.amount;
+            break;
+          case 'payout':
+            userData.payOut += entry.amount;
+            break;
+        }
+        console.debug(value);
+      }
+      //TODO: update balance
+      this.userStore.set(key, userData);
+    }
+
+    this.userStore.set(this.userStore.size.toString(), {
+      balance: 100,
+      earned: 100,
+      spent: 100,
+      payOut: 100,
+      paidOut: 100,
+    });
+  }
+}
+
+export type AggregatedUserData = {
+  balance: number;
+  earned: number;
+  spent: number;
+  payOut: number;
+  paidOut: number;
+};
+
+export type UserPayout = {
+  userId: string;
+  payoutAmount: number;
+};
+
+export type UserTransaction = {
+  id: string;
+  userId: string;
+  createdAt: string;
+  type: string;
+  amount: number;
+};
+
+export type UserTransactionData = {
+  items: UserTransaction[];
+  meta: {
+    totalItems: number;
+    itemCount: number;
+    itemsPerPage: number;
+    totalPages: number;
+    currentPage: number;
+  };
 }
